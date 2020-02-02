c05bc99ecfdff54241817e5bd5c6ae47490ddb21
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-02-01 17:03:01 -0800

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 /**
- * Functions to talk to the mosaic controller
+ * Functions to talk to the Dream Machine
  */
+//% weight=100 icon="\uf0eb"
 namespace dreamMachine {
     const PAUSE_PER_BYTE = 5
     const PAUSE_PER_UPDATE = 1000
@@ -13,29 +14,41 @@ namespace dreamMachine {
         Blue
     }
 
+    //% fixedInstances
     export class Pod {
         private index: number;
 
         /**
          * The brightness from 0..255
          */
-        brightness: number;
+        private _brightness: number;
         /**
          * The color as an RGB value
          */
-        color: number;
+        private _color: number;
+
+        private static pods: Pod[] = [];
+
+        static updatePods() {
+            for (const pod of Pod.pods) {
+                pod.update();
+            }
+        }
 
         constructor(index: number) {
             this.index = index;
-            this.brightness = 0;
-            this.color = 0;
+            this._brightness = 0xf0;
+            this._color = 0xf000f0;
+
+            // record in list of pods
+            Pod.pods.push(this);
         }
 
-        update() {
-            this.sendByte(this.encode(Channel.Intensity, this.brightness & 0xff));
-            this.sendByte(this.encode(Channel.Red, this.color & 0xff));
-            this.sendByte(this.encode(Channel.Green, (this.color >> 8) & 0xff));
-            this.sendByte(this.encode(Channel.Blue, (this.color >> 16) & 0xff));
+        private update() {
+            this.sendByte(this.encode(Channel.Intensity, this._brightness & 0xff));
+            this.sendByte(this.encode(Channel.Red, this._color & 0xff));
+            this.sendByte(this.encode(Channel.Green, (this._color >> 8) & 0xff));
+            this.sendByte(this.encode(Channel.Blue, (this._color >> 16) & 0xff));
         }
 
         private encode(channel: number, value: number) {
@@ -52,30 +65,51 @@ namespace dreamMachine {
             serial.writeBuffer(buf)
             pause(PAUSE_PER_BYTE)
         }
+
+        /**
+         * Sets the brightness of the pod, from 0..255
+         *
+         */
+        //% brightness.min=0 brightness.max=255  
+        //% blockId=dmpodsetbrightness
+        //% block="set pod %pod brightness to %brightness"
+        setBrightness(brightness: number) {
+            this._brightness = brightness | 0;
+        }
+
+        /**
+         * Sets the desired color of the pod
+         */
+        //% blockId=dmpodsetcolor
+        //% block="set pod %pod color to %brightness"
+        setColor(color: number) {
+            this._color = color | 0;
+        }
     }
 
-    let _pods: Pod[];
     /**
-     * Gets the pods
+     * The big pod
      */
-    export function pods(): Pod[] {
-        if (!_pods) {
-            serial.setBaudRate(BAUD_RATE);
-            _pods = [
-                new Pod(0),
-                new Pod(1),
-                new Pod(2),
-                new Pod(3)
-            ]
-            setInterval(updatePods, PAUSE_PER_UPDATE)
-        }
-        return _pods;
-    }
+    //% fixedInstance whenUsed block="pod 0"
+    export const pod0 = new Pod(0);
+    /**
+     * The pod 1
+     */
+    //% fixedInstance whenUsed block="pod 0"
+    export const pod1 = new Pod(1);
+    /**
+     * The pod 2
+     */
+    //% fixedInstance whenUsed block="pod 0"
+    export const pod2 = new Pod(2);
+    /**
+     * The pod 3
+     */
+    //% fixedInstance whenUsed block="pod 3"
+    export const pod3 = new Pod(3);
 
-    function updatePods() {
-        for(const pod of pods()) {
-            pod.update();
-        }
-    }
+    // init system
+    serial.setBaudRate(BAUD_RATE);
+    setInterval(Pod.updatePods, PAUSE_PER_UPDATE)
 }
 
